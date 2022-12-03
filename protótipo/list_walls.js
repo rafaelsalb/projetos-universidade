c17c988ec75a19ab2c1ca88f47c9661ff6a1253f
Rafael Albuquerque <61889057+rafaelsalb@users.noreply.github.com>
2022-12-03 16:30:09 -0300

    Add files via upload

--- protótipo/list_walls.js
+++ protótipo/list_walls.js
@@ -7,16 +7,16 @@ function list_walls()
         list += '"x1":"' + walls[i].a.x.toString() + '", "y1":"' + walls[i].a.y.toString() + '", "x2":"' + walls[i].b.x.toString() + '", "y2":"' + walls[i].b.y.toString() + '"';
         list += "}]' ";
         list += 'id="wall-' + i.toString() + '">';
-        list += '<input type="button" value="Remove" onclick="remove_wall(' + i.toString() + ')">';
         list += i.toString() + ':';
-        list += 'x1(<input id="wall-' + i.toString() + '-x1" "type="text" value="' + walls[i].a.x.toString() + '"> ';
-        list += 'y1(<input id="wall-' + i.toString() + '-y1" type="text" value="' + walls[i].a.y.toString() + '"> ';
-        list += 'x2(<input id="wall-' + i.toString() + '-x2" type="text" value="' + walls[i].b.x.toString() + '"> ';
-        list += 'y2(<input id="wall-' + i.toString() + '-y2" type="text" value="' + walls[i].b.y.toString() + '"> ';
-        list += 'Cor: ' + '<input id="wall-' + i.toString() + '-color" type="color" value="#FFFFFF"> ';
-        list += 'Altura: ' + '<input id="wall-' + i.toString() + '-height" type="text" value="' + walls[i].height + '"> ';
+        list += '<input id="wall-' + i.toString() + '-x1" placeholder="X1" type="text" value="' + walls[i].a.x.toString() + '" style="width: 20px"> ';
+        list += '<input id="wall-' + i.toString() + '-y1" placeholder="Y1" type="text" value="' + walls[i].a.y.toString() + '" style="width: 20px"> ';
+        list += '<input id="wall-' + i.toString() + '-x2" placeholder="X2" type="text" value="' + walls[i].b.x.toString() + '" style="width: 20px"> ';
+        list += '<input id="wall-' + i.toString() + '-y2" placeholder="Y2" type="text" value="' + walls[i].b.y.toString() + '" style="width: 20px"> ';
+        list += 'Cor: ' + '<input id="wall-' + i.toString() + '-color" type="color" value="#FFFFFF" style="width: 35px; height: 25px;"> ';
+        list += 'Altura (cm): ' + '<input id="wall-' + i.toString() + '-height" type="text" value="' + walls[i].height + '" style="width: 20px"> ';
         list += '<input type="button" value="Atualizar" onclick="update_wall(' + i.toString() + ')">';
-        list += '</div><br>';
+        list += '<input type="button" value="Remove" onclick="remove_wall(' + i.toString() + ')">';
+        list += '</div>';
     }
 
     document.getElementById("list_of_walls").innerHTML = list;
